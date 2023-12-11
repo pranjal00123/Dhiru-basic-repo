85af388527b99a2f67d9197e6e457b30f11be335
eloelo <dhiru90120@gmal.com>
2023-12-11 09:58:02 +0530

    variables and data types

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const remoteId = 12345;
+let remoteEmail = "sanjay@google.com"
+var remotePassword = "12345"
+
+// remoteId = 344566;
+/*
+    prefer not to use var 
+    because of issue in block scope and function scope
+*/
+
+remoteEmail= "dhiru@example.com"
+remotePassword = "4856759";
+
+console.table([remoteId, remoteEmail, remotePassword])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,27 @@
+let count = 33;
+
+console.log(count)
+
+console.log(typeof count)
+
+let number = 12345 // Number
+console.log(typeof number);
+
+let name = "Dhirendra" // string
+console.log(typeof name);
+
+let isLoggedIn = true; //boolean
+console.log(typeof isLoggedIn);
+
+let city;
+console.log(typeof city) // undefined
+
+let data = null;
+console.log(typeof data) // null
+
+//Object 
+
+
+// symbol
+
+
