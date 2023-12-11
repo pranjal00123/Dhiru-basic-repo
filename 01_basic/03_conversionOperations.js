766d3b7fd35db48e4bfdb9e01ff9f0951900c92e
eloelo <dhiru90120@gmail.com>
2023-12-11 20:12:49 +0530

    converstion data

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,65 @@
+
+// conversion into number-----------------------------------------------------------
+
+//let number = 1;
+// let number = "1jksdf"
+// let number = false;
+//let number = true;
+// let number = undefined;
+// let number = null;
+
+
+// let changeToNumber = Number(number)
+// console.log(typeof changeToNumber)
+// console.log(changeToNumber);
+
+
+// "1" => 1 number value = 1;
+// "1ans" => typeof change into number but value is NaN;
+// false => 0 number;
+// true => 1 number;
+// undefined => typeof change into number but value is NaN;
+// null => typeod change into number and value change into 0
+
+// coversition into string ----------------------------------------------------------------
+
+// let value = 1;
+// let value = false;
+// let value = true;
+// let value = undefined;
+// let value = null;
+
+
+// let changeIntoString = String(value);
+// console.log(typeof changeIntoString);
+// console.log(changeIntoString);
+
+
+// 1 => change into string and value is 1;
+// false => change into string and value is false;
+// true => change into string and value is true;
+// undefined => change into string and value is undefined
+// null => change into stind and value is null;
+
+
+// Conversion into boolean-----------------------------------------------
+
+let something = 1;
+
+let changeIntoBoolean = Boolean(something);
+console.log(typeof changeIntoBoolean);
+console.log(changeIntoBoolean);;
+
+
+// 1 => boolean / true
+// undefined => boolean / false;
+// null => boolean/ false;
+// empty string => boolean / false;
+// string => boolean / true;
+// boolean => boolean / true => true / false => false;
+// 0 => boolean / false
+
+
+
+
+
