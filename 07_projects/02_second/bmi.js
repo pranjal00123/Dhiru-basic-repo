66c24ca28c25efb3060b7756a69d6a6902d971f4
eloelo <dhiru90120@gmail.com>
2024-01-29 16:25:08 +0530

    live time clock

--- 07_projects/02_second/bmi.js
+++ 07_projects/02_second/bmi.js
@@ -6,12 +6,13 @@ form.addEventListener("submit", function(e){
     const height = parseInt(document.querySelector("#height").value);
     const weight = parseInt(document.querySelector("#weight").value);
     const result = document.querySelector(".result");
+    result.style.margin = "4px";
     if(height === "" || height < 0 || isNaN(height)){
         result.innerHTML = `please enter a valid height`
     } else if (weight === "" || weight < 0 || isNaN(weight)){
         result.innerHTML = `please enter a valid weight`
     } else {
         const calculatedResult = ((weight / ((height * height) / 10000)).toFixed(2));
-        result.innerHTML = calculatedResult
+        result.innerHTML = `<span>${calculatedResult}<span/>`
     }
 })
