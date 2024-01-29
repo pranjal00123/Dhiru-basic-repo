24a0829a49f6f2e9aab882c08c94af2bd33442df
eloelo <dhiru90120@gmail.com>
2024-01-29 13:49:02 +0530

    BMI calculator

---
+++ 07_projects/02_second/bmi.js
@@ -0,0 +1,17 @@
+document.querySelector("body").style.backgroundColor = "darkGrey"
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const height = parseInt(document.querySelector("#height").value);
+    const weight = parseInt(document.querySelector("#weight").value);
+    const result = document.querySelector(".result");
+    if(height === "" || height < 0 || isNaN(height)){
+        result.innerHTML = `please enter a valid height`
+    } else if (weight === "" || weight < 0 || isNaN(weight)){
+        result.innerHTML = `please enter a valid weight`
+    } else {
+        const calculatedResult = ((weight / ((height * height) / 10000)).toFixed(2));
+        result.innerHTML = calculatedResult
+    }
+})
